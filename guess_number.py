a860f58d80136be953dbcc4e159557c9ed737018
Kylan Plunk <kaplunk1@memphis.edu>
2024-02-26 12:21:18 -0600

    Added infinite guesses and a quit option

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,13 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+while(True):
+    guess = input("What number am I thinking of? ")
+    if guess == 'q':
+        print(f"Sorry! The number was {number}.")
+        break
+    elif int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        print("That's not it. Try again.")
